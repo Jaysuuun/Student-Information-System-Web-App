587ad4f7ed9839c5664aaaa2653cfebbc248ce45
Jaysuuun <bobjason.legaspi@g.msuiit.edu.ph>
2025-06-05 12:12:49 +0800

    begone venv

--- run.py
+++ run.py
@@ -1,6 +1,16 @@
+# from app import create_app
+# from dotenv import load_dotenv
+
+# load_dotenv('.env')
+
+# app = create_app()
+
 from app import create_app
 from dotenv import load_dotenv
 
 load_dotenv('.env')
 
 app = create_app()
+
+if __name__ == "__main__":
+    app.run(debug=True)
